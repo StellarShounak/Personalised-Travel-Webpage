ea0c18ea1e39f89cd1afa9bd2f2c107cc4a4eed7
Shounak Chattopadhyay <80070280+StellarShounak@users.noreply.github.com>
2023-04-21 23:32:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router,   Routes, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+        <Route path='/' exact element={<Home/>} />
+          <Route path='/services' element={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
